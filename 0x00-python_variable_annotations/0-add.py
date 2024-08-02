f86364b227a2046318e3fe6c8eb1baa05fdb60dd
kyaloeriq <kyaloeriq@gmail.com>
2024-08-02 11:32:37 -0700

    type-annotated function add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,7 @@ This module provides a function to add two float numbers.
 
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns their sum.
+    """
     return a + b
