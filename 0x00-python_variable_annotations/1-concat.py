40db03bf4274b79bf354be6b971be49796a64479
kyaloeriq <kyaloeriq@gmail.com>
2024-08-02 12:22:24 -0700

    type-annotated function concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This module provides functions to perform basic operations.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+    """
+    return str1 + str2
