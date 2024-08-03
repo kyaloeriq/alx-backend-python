109919b30faebee04bd4269ef4a3628a40469f03
kyaloeriq <kyaloeriq@gmail.com>
2024-08-02 23:00:21 -0700

    type-annotated function to_str

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module provides a function to convert a float to its string representation.
+Module provides a function to convert a float to its string rep.
 """
 
 
