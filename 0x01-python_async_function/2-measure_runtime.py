852dd329ee48659c68df8a708a08823eb8d8dcb2
kyaloeriq <kyaloeriq@gmail.com>
2024-08-05 12:36:27 -0700

    Measures the runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,15 +6,21 @@ Module to measure the runtime of asynchronous coroutines
 import asyncio
 import time
 import importlib.util
+import sys
 
-# Dynamically import wait_n from 0-basic_async_syntax.py
-spec = importlib.util.spec_from_file_location(
-        "basic_async_syntax", "./0-basic_async_syntax.py"
-        )
-basic_async_syntax = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(basic_async_syntax)
-wait_n = basic_async_syntax.wait_n
+def import_module_from_file(module_name: str, file_path: str):
+    """
+    Dynamically import a module from a file path.
+    """
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
 
+# Dynamically import wait_n from 1-concurrent_coroutines
+module = import_module_from_file("concurrent_coroutines", "./1-concurrent_coroutines")
+wait_n = getattr(module, "wait_n")
 
 def measure_time(n: int, max_delay: int) -> float:
     """
