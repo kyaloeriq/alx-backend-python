f789e303149c0c17869cb7e2ae6960d6dc6c8f3b
kyaloeriq <kyaloeriq@gmail.com>
2024-08-04 23:53:45 -0700

    a type-annotated function to_kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Union, Tuple
 """
 Model of a type-annotated function to_kv
 """
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
