223d3f19eba495e84ca3ce4aabf5c0991d94a157
kyaloeriq <kyaloeriq@gmail.com>
2024-08-05 12:07:37 -0700

    Measures the runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,11 +9,11 @@ from typing import List
 from 0-basic_async_syntax import wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the total execution time for wait_n(n, max_delay)
     """
     start_time = time.perf_counter()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     total_time = time.perf_counter() - start_time
     return total_time / n
