2fe8e34fa4fd34532c8bd18130b90ba04bf0818d
kyaloeriq <kyaloeriq@gmail.com>
2024-08-05 00:58:19 -0700

    an asynchronous coroutine that takes in an integer argument

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for an asynchronous coroutine that takes in an integer argument
+"""
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronous coroutine that waits for a random delay
+    """
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
