3ffd6a029edf0a1cb0978c09f719f2424839306e
kyaloeriq <kyaloeriq@gmail.com>
2024-08-09 12:07:31 -0700

    function task_wait_random that takes an integer max_delay and returns a asyncio.Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,6 +6,7 @@ Module to create an asyncio.Task from wait_random.
 import asyncio
 from 0-basic_async_syntax import wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Create and return an asyncio.Task that runs wait_random.
